73cf4d73d5bd0759757e27f7e31681e19bbbac3f
Tyler Whitehouse <tywhitehouse10@hotmail.com>
2015-03-26 23:18:01 -0400

    Please, God, let this work

--- Doctor Who Ticket To Ride/src/doctorWhoGame/GameStarter.java
+++ Doctor Who Ticket To Ride/src/doctorWhoGame/GameStarter.java
@@ -1,5 +1,19 @@
 package doctorWhoGame;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 public class GameStarter {
 
+	public static void main(String[] args){
+		JFrame window = new JFrame();
+		JPanel startScreen = new JPanel();
+		JButton startButton = new JButton("Start Game");
+		window.add(startScreen);
+		startScreen.add(startButton);
+		window.pack();
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setVisible(true);
+	}
 }
