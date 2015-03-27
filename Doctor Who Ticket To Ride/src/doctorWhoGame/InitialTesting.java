65c3e5481b5aaae422cf8c44ef12b88d9077a356
Tyler Whitehouse <tywhitehouse10@hotmail.com>
2015-03-26 23:55:12 -0400

    Adding tests

---
+++ Doctor Who Ticket To Ride/src/doctorWhoGame/InitialTesting.java
@@ -0,0 +1,58 @@
+package doctorWhoGame;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class InitialTesting {
+
+	@Test
+	public void testDeckPopulates() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testDeckShuffles() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testAddingPlayer() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testPathOwnership() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testPathLength(){
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testPlayerScoreIncrease() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testSuccessfulPathPurchase() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testFailedPathPurchase() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testIfPlayerHasTrains() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testIfPlayersTrainsDecrementAfterPathPurchase() {
+		fail("Not yet implemented");
+	}
+}
