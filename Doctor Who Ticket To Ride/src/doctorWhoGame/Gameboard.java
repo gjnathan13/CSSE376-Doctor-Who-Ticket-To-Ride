22666ea50f4070b6e52126d326c47cfdcbe54b8a
nathangj <nathangj@NATHANGJ-1.rose-hulman.edu>
2015-04-01 17:50:56 -0400

    Added an empty string file to pass checking for hand area file

--- Doctor Who Ticket To Ride/src/doctorWhoGame/Gameboard.java
+++ Doctor Who Ticket To Ride/src/doctorWhoGame/Gameboard.java
@@ -3,23 +3,20 @@ package doctorWhoGame;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
+import java.io.File;
 
 import javax.swing.JComponent;
 
 public class Gameboard extends JComponent {
-	private BufferedImage backgroundImage;
+	private File handAreaFile = new File("") ;
 
 	public Gameboard() {
-		
 	}
 
 	@Override
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		Graphics2D graphics2 = (Graphics2D) g;
-		graphics2.drawImage(this.backgroundImage, 0, 0,
-				this.backgroundImage.getWidth(),
-				this.backgroundImage.getHeight(), null);
 	}
 
 }
