e9c7524cbe8a85d4fd1adfb07c263587cada8152
wrightsd <wrightsd@rose-hulman.edu>
2015-03-31 22:49:42 -0400

    Added javadoc comments for hand

--- Doctor Who Ticket To Ride/src/doctorWhoGame/Hand.java
+++ Doctor Who Ticket To Ride/src/doctorWhoGame/Hand.java
@@ -1,9 +1,21 @@
 package doctorWhoGame;
 
+/**
+ * The hand object which contains many sets of cards. Each set is a different
+ * color or it is the route cards the player has, or it is the action cards the
+ * player has.
+ * 
+ * @author wrightsd
+ *
+ */
 public class Hand {
-	
-	public Hand(){
-		
+
+	/**
+	 * The constructor for the hand object that initializes all the different
+	 * sets of cards.
+	 */
+	public Hand() {
+
 	}
 
 }
