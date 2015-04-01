73f96f7542c24b3f0ff099c2cf51095996e11ee4
nathangj <nathangj@NATHANGJ-1.rose-hulman.edu>
2015-04-01 18:07:36 -0400

    Added test to check that hand area image in gameboard is actually coming from a read file

--- Doctor Who Ticket To Ride/src/doctorWhoGame/GameboardTester.java
+++ Doctor Who Ticket To Ride/src/doctorWhoGame/GameboardTester.java
@@ -4,11 +4,18 @@ import static org.junit.Assert.*;
 
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.IOException;
 import java.lang.reflect.*;
 
+import javax.imageio.ImageIO;
+
+import org.jmock.Mockery;
+import org.jmock.Expectations;
 import org.junit.*;
 
 public class GameboardTester {
+	
+	Mockery context = new Mockery();
 	
 	@Test
 	public void TestGameboardExists(){
@@ -39,5 +46,25 @@ public class GameboardTester {
 		BufferedImage handAreaImage = (BufferedImage) privateHandAreaImage.get(gameScreen);
 		assertNotNull(handAreaImage);
 	}
+	
+	@Test
+	public void TestThatGameboardIsPerformingFileIOToGetHandAreaImage() throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException, IOException{
+		final ImageIO imageInputOutputClass = context.mock(ImageIO.class);
+		Gameboard gameScreen = new Gameboard();
+		Field privateHandAreaImageFile = Gameboard.class.
+	            getDeclaredField("handAreaFile");
+		
+		privateHandAreaImageFile.setAccessible(true);
+		
+		final File handAreaFile = (File) privateHandAreaImageFile.get(gameScreen);
+		
+		context.checking(new Expectations() {{ oneOf (imageInputOutputClass).read(handAreaFile); }} );
+		
+		context.assertIsSatisfied();
+	}
+	
+	
+	
+	
 
 }
