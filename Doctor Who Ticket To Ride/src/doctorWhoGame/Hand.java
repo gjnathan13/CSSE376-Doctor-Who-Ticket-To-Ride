c386e58cf1288c5531dbc45953b386020cf41f30
wrightsd <wrightsd@rose-hulman.edu>
2015-04-01 17:08:13 -0400

    tests of addTrainCards all run

--- Doctor Who Ticket To Ride/src/doctorWhoGame/Hand.java
+++ Doctor Who Ticket To Ride/src/doctorWhoGame/Hand.java
@@ -5,8 +5,8 @@ import java.util.ArrayList;
 /**
  * The hand object which contains many sets of cards. Each set is a different
  * color or it is the route cards the player has, or it is the action cards the
- * player has. The train card sets in the hand go in the order: 0-RED 1-PINK 2-
- * ORANGE 3- YELLOW 4-GREEN 5-BLUE 6-WHITE 7-BLACK 8-RAINBOW
+ * player has. The train card sets in the hand go in the order: 0-Red 1-Pink 2-
+ * Orange 3- Yellow 4-Green 5-Blue 6-White 7-Black 8-Rainbow
  * 
  * @author wrightsd
  *
@@ -27,11 +27,54 @@ public class Hand {
 		}
 	}
 
+	/**
+	 * Adds the given train card color string to the correct sub-ArrayList of
+	 * the train colors ArrayList. Timothy Anderson gave me the idea to use a
+	 * switching function.
+	 * 
+	 * @param trainCard
+	 *            String that is the color of the train card with the first
+	 *            letter capitalized. The options are Red, Pink, Orange, Yellow,
+	 *            Green, Blue, White, Black, and Rainbow.
+	 */
 	public void addTrainCard(String trainCard) {
-		if (trainCard.equals("Pink")) {
-			this.trainCards.get(1).add(trainCard);
-		} else {
+		switch (trainCard) {
+		case "Red": {
 			this.trainCards.get(0).add(trainCard);
+			break;
+		}
+		case "Pink": {
+			this.trainCards.get(1).add(trainCard);
+			break;
+		}
+		case "Orange": {
+			this.trainCards.get(2).add(trainCard);
+			break;
+		}
+		case "Yellow": {
+			this.trainCards.get(3).add(trainCard);
+			break;
+		}
+		case "Green": {
+			this.trainCards.get(4).add(trainCard);
+			break;
+		}
+		case "Blue": {
+			this.trainCards.get(5).add(trainCard);
+			break;
+		}
+		case "White": {
+			this.trainCards.get(6).add(trainCard);
+			break;
+		}
+		case "Black": {
+			this.trainCards.get(7).add(trainCard);
+			break;
+		}
+		case "Rainbow": {
+			this.trainCards.get(8).add(trainCard);
+			break;
+		}
 		}
 
 	}
--- Doctor Who Ticket To Ride/src/doctorWhoGame/HandTesting.java
+++ Doctor Who Ticket To Ride/src/doctorWhoGame/HandTesting.java
@@ -3,61 +3,152 @@ package doctorWhoGame;
 import static org.junit.Assert.*;
 
 import java.lang.reflect.Field;
-import java.lang.reflect.Method;
 import java.util.ArrayList;
-
-import junit.framework.Assert;
 
 import org.junit.Before;
 import org.junit.Test;
 
+/**
+ * Tests the Hand class
+ * 
+ * @author wrightsd
+ *
+ */
 public class HandTesting {
 	private ArrayList<ArrayList<String>> trainCardList;
 	private Hand newHand;
-	
-	//Sets up local variables
+
+	/**
+	 * Sets up local variables
+	 * 
+	 * @throws NoSuchFieldException
+	 * @throws SecurityException
+	 * @throws IllegalArgumentException
+	 * @throws IllegalAccessException
+	 */
+	@SuppressWarnings("unchecked")
 	@Before
-	public void testSetup() throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException{
-	this.newHand=new Hand();
-	Field trainCardField=Hand.class.getDeclaredField("trainCards");
-	trainCardField.setAccessible(true);
-	this.trainCardList= (ArrayList<ArrayList<String>>) trainCardField.get(newHand);
-	
+	public void testSetup() throws NoSuchFieldException, SecurityException,
+			IllegalArgumentException, IllegalAccessException {
+		this.newHand = new Hand();
+		Field trainCardField = Hand.class.getDeclaredField("trainCards");
+		trainCardField.setAccessible(true);
+		this.trainCardList = (ArrayList<ArrayList<String>>) trainCardField
+				.get(newHand);
+
 	}
-	
-	//Tests the constructor
+
+	/**
+	 * Tests the constructor
+	 */
 	@Test
-	public void testThatHandExists(){
-		Hand trainCardHand=new Hand();
-		
+	public void testThatHandExists() {
+		assertNotEquals(new Hand(), null);
+
 	}
-	
-	//Tests adding a red card to the hand
+
+	/**
+	 * Tests adding a red card to the hand
+	 */
 	@Test
-	public void testAddRedTrainCard(){
-		String firstTrainCard="Red";
+	public void testAddRedTrainCard() {
+		String firstTrainCard = "Red";
 		newHand.addTrainCard(firstTrainCard);
 		assertEquals(1, trainCardList.get(0).size());
-		assertEquals(firstTrainCard,trainCardList.get(0).get(0));
-		for(int i=1;i<9;i++){
+		assertEquals(firstTrainCard, trainCardList.get(0).get(0));
+		for (int i = 1; i < 9; i++) {
 			assertEquals(0, trainCardList.get(i).size());
 		}
 	}
-	
-	//Tests adding a pink card to the hand
+
+	/**
+	 * Tests adding a pink card to the hand
+	 */
 	@Test
-	public void testAddPinkTrainCard(){
-		String firstTrainCard="Pink";
+	public void testAddPinkTrainCard() {
+		String firstTrainCard = "Pink";
 		newHand.addTrainCard(firstTrainCard);
 		assertEquals(1, trainCardList.get(1).size());
-		assertEquals(firstTrainCard,trainCardList.get(1).get(0));
+		assertEquals(firstTrainCard, trainCardList.get(1).get(0));
 		assertEquals(0, trainCardList.get(0).size());
-		for(int i=2;i<9;i++){
+		for (int i = 2; i < 9; i++) {
 			assertEquals(0, trainCardList.get(i).size());
 		}
 	}
-	
-	
-	
+
+	/**
+	 * Tests adding many cards of different colors
+	 */
+	@Test
+	public void testAddManyDifferentColorTrainCard() {
+		String redTrainCard = "Red";
+		String pinkTrainCard = "Pink";
+		String orangeTrainCard = "Orange";
+		String yellowTrainCard = "Yellow";
+		String greenTrainCard = "Green";
+		String blueTrainCard = "Blue";
+		String whiteTrainCard = "White";
+		String blackTrainCard = "Black";
+		String rainbowTrainCard = "Rainbow";
+		for (int i = 0; i < 9; i++) {
+			newHand.addTrainCard(redTrainCard);
+		}
+		for (int i = 0; i < 8; i++) {
+			newHand.addTrainCard(pinkTrainCard);
+		}
+		for (int i = 0; i < 7; i++) {
+			newHand.addTrainCard(orangeTrainCard);
+		}
+		for (int i = 0; i < 6; i++) {
+			newHand.addTrainCard(yellowTrainCard);
+		}
+		for (int i = 0; i < 5; i++) {
+			newHand.addTrainCard(greenTrainCard);
+		}
+		for (int i = 0; i < 4; i++) {
+			newHand.addTrainCard(blueTrainCard);
+		}
+		for (int i = 0; i < 3; i++) {
+			newHand.addTrainCard(whiteTrainCard);
+		}
+		for (int i = 0; i < 2; i++) {
+			newHand.addTrainCard(blackTrainCard);
+		}
+		newHand.addTrainCard(rainbowTrainCard);
+		
+		//Test that each list is the right length
+		for (int i = 9; i > 0; i--) {
+			int indexOfList=9-i;
+			assertEquals(i, trainCardList.get(indexOfList).size());
+		}
+		
+		//Test that each list contains the right things
+		for (int i = 0; i < 9; i++) {
+			assertEquals(redTrainCard, trainCardList.get(0).get(i));
+		}
+		for (int i = 0; i < 8; i++) {
+			assertEquals(pinkTrainCard, trainCardList.get(1).get(i));
+		}
+		for (int i = 0; i < 7; i++) {
+			assertEquals(orangeTrainCard, trainCardList.get(2).get(i));
+		}
+		for (int i = 0; i < 6; i++) {
+			assertEquals(yellowTrainCard, trainCardList.get(3).get(i));
+		}
+		for (int i = 0; i < 5; i++) {
+			assertEquals(greenTrainCard, trainCardList.get(4).get(i));
+		}
+		for (int i = 0; i < 4; i++) {
+			assertEquals(blueTrainCard, trainCardList.get(5).get(i));
+		}
+		for (int i = 0; i < 3; i++) {
+			assertEquals(whiteTrainCard, trainCardList.get(6).get(i));
+		}
+		for (int i = 0; i < 2; i++) {
+			assertEquals(blackTrainCard, trainCardList.get(7).get(i));
+		}
+		assertEquals(rainbowTrainCard, trainCardList.get(8).get(0));
+
+	}
 
 }
