74cf0ea69d32e49176dbe54da48659bff181d263
wrightsd <wrightsd@rose-hulman.edu>
2015-04-01 16:23:10 -0400

    Redid testAddRedCard so that it has an @Before to use for every adding card test

--- Doctor Who Ticket To Ride/src/doctorWhoGame/HandTesting.java
+++ Doctor Who Ticket To Ride/src/doctorWhoGame/HandTesting.java
@@ -8,25 +8,37 @@ import java.util.ArrayList;
 
 import junit.framework.Assert;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class HandTesting {
+	private ArrayList<ArrayList<String>> trainCardList;
+	private Hand newHand;
+	
+	@Before
+	public void testSetup() throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException{
+	this.newHand=new Hand();
+	Field trainCardField=Hand.class.getDeclaredField("trainCards");
+	trainCardField.setAccessible(true);
+	this.trainCardList= (ArrayList<ArrayList<String>>) trainCardField.get(newHand);
+	
+	}
 	
 	@Test
 	public void testThatHandExists(){
 		Hand trainCardHand=new Hand();
+		
 	}
 	
+	
 	@Test
-	public void testAddRedCard() throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException{
-		Hand newHand=new Hand();
+	public void testAddRedCard(){
 		String firstTrainCard="Red";
 		newHand.addTrainCard(firstTrainCard);
-		Field trainCardField=Hand.class.getDeclaredField("trainCards");
-		trainCardField.setAccessible(true);
-		ArrayList<ArrayList<String>> trainCardList= (ArrayList<ArrayList<String>>) trainCardField.get(newHand);
 		assertEquals(1, trainCardList.get(0).size());
 		assertEquals(firstTrainCard,trainCardList.get(0).get(0));
 	}
+	
+	
 
 }
