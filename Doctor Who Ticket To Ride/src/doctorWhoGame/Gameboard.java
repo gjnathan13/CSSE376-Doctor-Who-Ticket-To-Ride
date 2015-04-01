98c2c6bf695fe2ccbead4dbb1d12fdda9ba8d69c
nathangj <nathangj@NATHANGJ-1.rose-hulman.edu>
2015-04-01 17:59:15 -0400

    handAreaImage field added

--- Doctor Who Ticket To Ride/src/doctorWhoGame/Gameboard.java
+++ Doctor Who Ticket To Ride/src/doctorWhoGame/Gameboard.java
@@ -4,11 +4,14 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
 import javax.swing.JComponent;
 
 public class Gameboard extends JComponent {
 	private File handAreaFile = new File("") ;
+	private BufferedImage handAreaImage = new BufferedImage(1, 1, 1);
 
 	public Gameboard() {
 	}
