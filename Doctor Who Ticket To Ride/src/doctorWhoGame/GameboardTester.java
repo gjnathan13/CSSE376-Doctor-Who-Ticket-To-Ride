413fd17bb33540b85ca2d7592ebc6cb0d7b5371e
nathangj <nathangj@NATHANGJ-1.rose-hulman.edu>
2015-04-01 17:48:21 -0400

    Added test to check for hand area file, fails

--- Doctor Who Ticket To Ride/src/doctorWhoGame/GameboardTester.java
+++ Doctor Who Ticket To Ride/src/doctorWhoGame/GameboardTester.java
@@ -1,6 +1,10 @@
 package doctorWhoGame;
 
 import static org.junit.Assert.*;
+
+import java.io.File;
+import java.lang.reflect.*;
+
 import org.junit.*;
 
 public class GameboardTester {
@@ -8,6 +12,19 @@ public class GameboardTester {
 	@Test
 	public void TestGameboardExists(){
 		assertNotNull(new Gameboard());
+	}
+	
+	@Test
+	public void TestGameboardHasHandAreaImageFile() throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException{
+		Gameboard gameScreen = new Gameboard();
+		Field privateHandAreaImageFile = Gameboard.class.
+	            getDeclaredField("handAreaFile");
+		
+		privateHandAreaImageFile.setAccessible(true);
+		
+		File handAreaFile = (File) privateHandAreaImageFile.get(gameScreen);
+		assertNotNull(handAreaFile);
+
 	}
 
 }
