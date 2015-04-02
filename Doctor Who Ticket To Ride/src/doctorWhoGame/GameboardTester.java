73e2b07622551948a65fed8ababfd725677cb92b
nathangj <nathangj@NATHANGJ-1.rose-hulman.edu>
2015-04-01 20:24:58 -0400

    Test eliminated due to faulty logic, code reverted back to passing initial two tests

--- Doctor Who Ticket To Ride/src/doctorWhoGame/GameboardTester.java
+++ Doctor Who Ticket To Ride/src/doctorWhoGame/GameboardTester.java
@@ -5,17 +5,24 @@ import static org.junit.Assert.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.lang.reflect.*;
+import java.lang.reflect.Field;
 
 import javax.imageio.ImageIO;
 
-import org.jmock.Mockery;
-import org.jmock.Expectations;
-import org.junit.*;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.api.easymock.PowerMock;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
+import static org.easymock.EasyMock.*;
+import static org.junit.Assert.*;
+import static org.powermock.api.easymock.PowerMock.*;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(ImageIO.class)
 public class GameboardTester {
-	
-	Mockery context = new Mockery();
 	
 	@Test
 	public void TestGameboardExists(){
@@ -45,22 +52,6 @@ public class GameboardTester {
 		
 		BufferedImage handAreaImage = (BufferedImage) privateHandAreaImage.get(gameScreen);
 		assertNotNull(handAreaImage);
-	}
-	
-	@Test
-	public void TestThatGameboardIsPerformingFileIOToGetHandAreaImage() throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException, IOException{
-		final ImageIO imageInputOutputClass = context.mock(ImageIO.class);
-		Gameboard gameScreen = new Gameboard();
-		Field privateHandAreaImageFile = Gameboard.class.
-	            getDeclaredField("handAreaFile");
-		
-		privateHandAreaImageFile.setAccessible(true);
-		
-		final File handAreaFile = (File) privateHandAreaImageFile.get(gameScreen);
-		
-		context.checking(new Expectations() {{ oneOf (imageInputOutputClass).read(handAreaFile); }} );
-		
-		context.assertIsSatisfied();
 	}
 	
 	
