6ec395ca1386287c034fb36f8c1b0fbff3e062c9
nathangj <nathangj@NATHANGJ-1.rose-hulman.edu>
2015-04-16 19:19:45 -0400

    Added test to check for image file in route board, added paramter in Routeboard so test runs, but fails

--- Doctor Who Ticket To Ride/src/doctorWhoGame/Routeboard.java
+++ Doctor Who Ticket To Ride/src/doctorWhoGame/Routeboard.java
@@ -1,7 +1,10 @@
 package doctorWhoGame;
 
 import java.awt.Panel;
+import java.io.File;
 
 public class Routeboard extends Panel {
+	
+	private File routeBackFile;
 
 }
--- Doctor Who Ticket To Ride/src/doctorWhoGame/RouteboardTester.java
+++ Doctor Who Ticket To Ride/src/doctorWhoGame/RouteboardTester.java
@@ -2,6 +2,11 @@ package doctorWhoGame;
 
 import static org.junit.Assert.assertNotNull;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.lang.reflect.Field;
+
+import org.junit.Before;
 import org.junit.Test;
 
 /**
@@ -12,12 +17,48 @@ import org.junit.Test;
  */
 public class RouteboardTester {
 	
+	private Routeboard routeScreen;
+	private File routeBackFile;
+	
+	/**
+	 * Grabs all private fields from a Routeboard instance and assigns them to
+	 * fields for use in tests.
+	 * 
+	 * @throws NoSuchFieldException
+	 * @throws SecurityException
+	 * @throws IllegalArgumentException
+	 * @throws IllegalAccessException
+	 */
+	@Before
+	public void InitializingVariables() throws NoSuchFieldException,
+			SecurityException, IllegalArgumentException, IllegalAccessException {
+		this.routeScreen = new Routeboard();
+
+		Field privateRouteBack = Routeboard.class
+				.getDeclaredField("routeBackFile");
+
+		privateRouteBack.setAccessible(true);
+		this.routeBackFile = (File) privateRouteBack.get(routeScreen);
+
+	}
+
+	
 	/**
 	 * Makes sure a Routeboard can be initialized.
 	 */
 	@Test
-	public void TestGameboardExists() {
+	public void TestRouteboardExists() {
 		assertNotNull(new Routeboard());
+	}
+	
+	/**
+	 * Ensures that the Gameboard has an image file for the area to display the
+	 * cards in the current hand.
+	 */
+	@Test
+	public void TestRouteboardHasHandAreaImageFile() {
+		assertNotNull(routeBackFile);
+
 	}
 }
 
