32d7a9b6682da98b2a5b62b1f0c8e38b3599b3ab
gjnathan13 <nathangj@rose-hulman.edu>
2015-04-17 00:57:26 -0400

    Final changes before merging

--- Doctor Who Ticket To Ride/src/doctorWhoGame/RouteboardTester.java
+++ Doctor Who Ticket To Ride/src/doctorWhoGame/RouteboardTester.java
@@ -18,16 +18,16 @@ import org.junit.Test;
  * Test the non-gui related functionality of Routeboard
  * 
  * @author nathangj on 4/16/15
- *
+ * 
  */
 public class RouteboardTester {
-	
+
 	private Routeboard routeScreen;
 	private File routeBackFile;
 	private BufferedImage routeBackImage;
 	private int imageWidth;
 	private int imageHeight;
-	
+
 	/**
 	 * Grabs all private fields from a Routeboard instance and assigns them to
 	 * fields for use in tests.
@@ -47,18 +47,18 @@ public class RouteboardTester {
 
 		privateRouteBack.setAccessible(true);
 		this.routeBackFile = (File) privateRouteBack.get(routeScreen);
-		
+
 		Field privateRouteBackImage = Routeboard.class
 				.getDeclaredField("routeBackImage");
 
 		privateRouteBackImage.setAccessible(true);
-		this.routeBackImage = (BufferedImage) privateRouteBackImage.get(routeScreen);
-		
+		this.routeBackImage = (BufferedImage) privateRouteBackImage
+				.get(routeScreen);
+
 		this.imageWidth = this.routeBackImage.getWidth();
 		this.imageHeight = this.routeBackImage.getHeight();
 	}
 
-	
 	/**
 	 * Makes sure a Routeboard can be initialized.
 	 */
@@ -66,7 +66,7 @@ public class RouteboardTester {
 	public void TestRouteboardExists() {
 		assertNotNull(new Routeboard());
 	}
-	
+
 	/**
 	 * Ensures that the Routeboard has an image file for the area to display the
 	 * cards in the current hand.
@@ -75,7 +75,7 @@ public class RouteboardTester {
 	public void TestRouteboardHasHandAreaImageFile() {
 		assertNotNull(routeBackFile);
 	}
-	
+
 	/**
 	 * Ensures the Routeboard has an actual image for displaying routes on.
 	 */
@@ -83,34 +83,40 @@ public class RouteboardTester {
 	public void TestRouteboardHasHandAreaImage() {
 		assertNotNull(routeBackImage);
 	}
-	
+
 	/**
 	 * Tests that Routeboard has a component on it
 	 */
 	@Test
-	public void TestRouteboardHasComponent(){
+	public void TestRouteboardHasComponent() {
 		Component[] componentList = routeScreen.getComponents();
 		assertTrue(componentList.length > 0);
 	}
-	
+
+	/**
+	 * Tests that Routeboard has a component on it with the correct image.
+	 */
 	@Test
-	public void TestRouteboardComponentIsJLabelWithRouteImage(){
+	public void TestRouteboardComponentIsJLabelWithRouteImage() {
 		Component[] componentList = routeScreen.getComponents();
 		assertTrue(componentList[0].getClass().equals(JLabel.class));
 		ImageIcon testIcon = new ImageIcon(routeBackImage);
-		
+
 		JLabel routeComponent = (JLabel) componentList[0];
 		ImageIcon routeImageIcon = (ImageIcon) routeComponent.getIcon();
-		
+
 		assertEquals(testIcon.getImage(), routeImageIcon.getImage());
 	}
-	
+
+	/**
+	 * Tests that the dimensions of the image associated with Routeboard can be
+	 * accessed with a getter.
+	 */
 	@Test
-	public void TestThatImageDimensionsCanBeAccessedWithGetter(){
+	public void TestThatImageDimensionsCanBeAccessedWithGetter() {
 		int[] testingRouteImageDimensions = this.routeScreen
 				.getRouteImageDimensions();
 		assertEquals((int) imageWidth, testingRouteImageDimensions[0]);
 		assertEquals((int) imageHeight, testingRouteImageDimensions[1]);
 	}
 }
-
