dc3900b0f7c87d9a441b049e37b17d9b806815b3
nathangj <nathangj@NATHANGJ-1.rose-hulman.edu>
2015-04-29 22:08:22 -0400

    Added code to pass test, getter for node point

--- Doctor Who Ticket To Ride/src/doctorWhoGame/Node.java
+++ Doctor Who Ticket To Ride/src/doctorWhoGame/Node.java
@@ -1,15 +1,27 @@
 package doctorWhoGame;
 
+import java.awt.Point;
+import java.awt.geom.Point2D;
+import java.awt.geom.Point2D.Double;
+
 public class Node {
 
 	private final int id;
+	private int xCoord;
+	private int yCoord;
 	
-	public Node(int i) {
+	public Node(int i, int x, int y) {
 		this.id = i;
+		this.xCoord = x;
+		this.yCoord = y;
 	}
 
 	public int getID() {
 		return id;
+	}
+	
+	public Point2D getNodePoint() {
+		return new Point2D.Double(xCoord, yCoord);
 	}
 
 }
