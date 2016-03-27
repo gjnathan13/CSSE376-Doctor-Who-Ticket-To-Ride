7d6e868dd5bc2653108ae355843ae01e7d9eb2a1
Emily Richardson <richarel@rose-hulman.edu>
2016-03-27 14:42:01 -0400

    EndGameComponentResolution fixes

--- Doctor Who Ticket To Ride/src/doctorWhoGame/EndGameComponent.java
+++ Doctor Who Ticket To Ride/src/doctorWhoGame/EndGameComponent.java
@@ -14,9 +14,9 @@ public class EndGameComponent extends JComponent {
 
 	private Graphics2D pen;
 	private boolean gameOver = false;
-	private final int OFFSET_X = 250;
-	private final int LABEL_WIDTH = 750;
-	private final int LABEL_HEIGHT = 75;
+	private final int OFFSET_X = (int)(GameStarter.getWidthModifier()*250);
+	private final int LABEL_WIDTH = (int)(GameStarter.getWidthModifier()*750);
+	private final int LABEL_HEIGHT = (int)(GameStarter.getHeightModifier()*75);
 	private int labelSpacing;
 	
 	@Override
@@ -24,11 +24,11 @@ public class EndGameComponent extends JComponent {
 		super.paintComponent(g);
 		this.pen = (Graphics2D) g;
 		pen.setColor(Color.BLACK);
-		pen.fillRect(0, 0, this.getWidth(), this.getHeight());
+		pen.fillRect(0, 0, (int)(GameStarter.getWidthModifier()*this.getWidth()), (int)(GameStarter.getHeightModifier()*this.getHeight()));
 		if (this.gameOver) {
 			ArrayList<Player> players = Game.getPlayerList();
-			Font playerFont = new Font("ISOCTEUR", Font.PLAIN, 36);
-			labelSpacing = (this.getHeight() - players.size() * LABEL_HEIGHT) / (players.size() + 1);
+			Font playerFont = new Font("ISOCTEUR", Font.PLAIN, (int)(GameStarter.getDiagonalModifier()*36));
+			labelSpacing = (int)(GameStarter.getHeightModifier() * (this.getHeight() - players.size() * LABEL_HEIGHT) / (players.size() + 1));
 			for (int i = 0; i < players.size(); i++) {
 				JLabel playerDisplay = new JLabel(players.get(i).getName() + ": " + players.get(i).getScore(),
 						JLabel.CENTER);
