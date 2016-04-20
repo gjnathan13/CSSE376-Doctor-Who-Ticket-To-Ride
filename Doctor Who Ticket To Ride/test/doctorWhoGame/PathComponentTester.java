e5cacd88011e438cffbddc9957350315bc2d38be
QuantumMenace <tmthyndrsn@gmail.com>
2016-04-19 22:38:43 -0400

    Tim: tweaked pathcomponenttester to remove unnecessary imports and clean up code

--- Doctor Who Ticket To Ride/test/doctorWhoGame/PathComponentTester.java
+++ Doctor Who Ticket To Ride/test/doctorWhoGame/PathComponentTester.java
@@ -1,15 +1,36 @@
 package doctorWhoGame;
 
+import static org.junit.Assert.*;
+
+import java.awt.Color;
 import java.awt.geom.Line2D;
 import java.awt.geom.Point2D;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 import org.easymock.EasyMock;
-import org.junit.Assert;
 import org.junit.Test;
 
 public class PathComponentTester {
+
+	@Test
+	public void testPathComponentConstructor() {
+		Node n1 = new Node(1, 2, 5);
+		Node n2 = new Node(2, 5, 2);
+		Node n3 = new Node(3, 22, 5);
+		Node n4 = new Node(4, 5, 22);
+		Path p1 = new Path(n1, n2, Color.RED, 5, 3);
+		Path p2 = new Path(n3, n4, Color.RED, 5, 0);
+		Path[] pathArray = { p1, p2 };
+		Node[] nodeArray = { n1, n2, n3, n4 };
+		Gameboard gb = new Gameboard();
+		PathComponent pathComponent = new PathComponent(pathArray, nodeArray, gb);
+		Line2D.Double line = new Line2D.Double(p1.getNodes()[0].getNodePoint(), p1.getNodes()[1].getNodePoint());
+		assertFalse(line.equals(p1.getLine()));
+		line = new Line2D.Double(p2.getNodes()[0].getNodePoint(), p2.getNodes()[1].getNodePoint());
+		assertEquals(line.getP1(), p2.getLine().getP1());
+		assertEquals(line.getP2(), p2.getLine().getP2());
+	}
 
 	@Test
 	public void TestPositiveShiftOnPathWithPositiveSlope() throws NoSuchMethodException, SecurityException,
@@ -27,10 +48,10 @@ public class PathComponentTester {
 		double y1Expect = 4.45;
 		double y2Expect = 7.45;
 
-		Assert.assertEquals(x1Expect, shiftedLine.getX1(), 0.01);
-		Assert.assertEquals(x2Expect, shiftedLine.getX2(), 0.01);
-		Assert.assertEquals(y1Expect, shiftedLine.getY1(), 0.01);
-		Assert.assertEquals(y2Expect, shiftedLine.getY2(), 0.01);
+		assertEquals(x1Expect, shiftedLine.getX1(), 0.01);
+		assertEquals(x2Expect, shiftedLine.getX2(), 0.01);
+		assertEquals(y1Expect, shiftedLine.getY1(), 0.01);
+		assertEquals(y2Expect, shiftedLine.getY2(), 0.01);
 	}
 
 	@Test
@@ -49,9 +70,9 @@ public class PathComponentTester {
 		double y1Expect = 4.45;
 		double y2Expect = 7.45;
 
-		Assert.assertEquals(x1Expect, shiftedLine.getX1(), 0.01);
-		Assert.assertEquals(x2Expect, shiftedLine.getX2(), 0.01);
-		Assert.assertEquals(y1Expect, shiftedLine.getY1(), 0.01);
-		Assert.assertEquals(y2Expect, shiftedLine.getY2(), 0.01);
+		assertEquals(x1Expect, shiftedLine.getX1(), 0.01);
+		assertEquals(x2Expect, shiftedLine.getX2(), 0.01);
+		assertEquals(y1Expect, shiftedLine.getY1(), 0.01);
+		assertEquals(y2Expect, shiftedLine.getY2(), 0.01);
 	}
 }
